fffa6636c045c03f2f52930c59604146366d34f5
Alex Kizilov <as_k@list.ru>
2022-10-11 22:36:24 +0400

    Example005_ConditionIfElse

---
+++ Examle005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "алексей")
+{
+    Console.WriteLine("Ура, это же Алексей!!!");
+}    
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
